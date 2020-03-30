ee07d9218e1c7b429987daae7a1b1ef87bee9133
hywook4 <hywook4@naver.com>
2020-03-30 13:08:32 +0900

    fixing image container

--- src/components/common/ImageCard.js
+++ src/components/common/ImageCard.js
@@ -9,12 +9,36 @@ export class ImageCard extends Component{
     }
 
     render(){
-        let width = (this.props.parentWidth - 80)/3;
+        const space = 50;
+        let colNum = 0;
+        let width = 0;
+
+        if(this.props.parentWidth<=400){
+            colNum = 1;
+
+        } else if(this.props.parentWidth>400 && this.props.parentWidth<=800){
+            colNum = 2;
+        } else if(this.props.parentWidth>800 && this.props.parentWidth<=1200){
+            colNum = 3;
+        } else if(this.props.parentWidth>1200 && this.props.parentWidth<=1600){
+            colNum = 4;
+        } else if(this.props.parentWidth>1600 && this.props.parentWidth<=2000){
+            colNum = 5;
+        } else{
+            colNum = 6;
+        }
+
+        width = (this.props.parentWidth-space*colNum)/colNum;
+
+        //let width = (this.props.parentWidth - 77)/3;
         let height = width*2;
         let cardStyle = {
             width: String(width)+"px",
             height: String(height)+"px"
         }
+
+
+
 
         return(
             <div className="imagecard_body" style={cardStyle}>
