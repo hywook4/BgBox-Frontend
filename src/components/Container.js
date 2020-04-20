c82ce51dc3f275fd3cd1c3ca4dfc9727fd32ab64
hywook4 <hywook4@naver.com>
2020-04-20 23:37:56 +0900

    router added, imagespec page start

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,20 +4,27 @@ import './Container.css';
 import {Header} from './contents/header/Header';
 import {Footer} from './contents/footer/Footer';
 import {Home} from './navigator/Home';
+import {ImageSpec} from './navigator/ImageSpec';
+
+import {Router, Route, Switch} from "react-router-dom";
+import createBrowserHistory from 'history/createBrowserHistory';
 
 
 export class Container extends Component{
-
 
 
     render(){
         return(
             <div id="main_body">
                 <Header/>
-
-                <Home/>
-                
-            
+                <div>
+                    <Router history={createBrowserHistory()}>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/images/:id" component={ImageSpec}/>
+                        </Switch>
+                    </Router>   
+                </div>
                 <Footer/>
             </div>
         )
